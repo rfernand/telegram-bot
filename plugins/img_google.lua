fc63d902484a9d211abb671adfb2ff483f597520
Rodrigo Fernandez <chaotiklv@gmail.com>
2015-05-13 16:35:00 -0300

    Trying fix for img plugin

--- plugins/img_google.lua
+++ plugins/img_google.lua
@@ -17,12 +17,32 @@ function getGoogleImage(text)
 
 end
 
+-- Download the image and send to receiver, it will be deleted.
+-- cb_function and cb_extra are optionals callback
+-- It will retry the command after failing
+function send_photo_from_url_with_retry(receiver, url, cb_function, cb_extra)
+  -- If callback not provided
+  cb_function = cb_function or ok_cb
+  cb_extra = cb_extra or false
+  
+  local file_path = download_to_file(url, false)
+  if not file_path then -- Error
+    -- 'Error downloading the image'
+    local text = msg.text:sub(6,-1)
+    local url = getGoogleImage(text)
+    send_photo_from_url_with_retry(receiver, url)
+  else
+    print("File path: "..file_path)
+    _send_photo(receiver, file_path, cb_function, cb_extra)
+  end
+end
+
 function run(msg, matches)
   local receiver = get_receiver(msg)
   local text = msg.text:sub(6,-1)
   local url = getGoogleImage(text)
   print("Image URL: ", url)
-  send_photo_from_url(receiver, url)
+  send_photo_from_url_with_retry(receiver, url)
 end
 
 return {
